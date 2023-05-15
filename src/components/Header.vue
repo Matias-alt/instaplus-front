92a3b03c7efeb343127096575f877437db9b1f74
Matias-alt <mat.altamirano.22@gmail.com>
2023-05-14 21:51:57 -0400

    main views

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Contenido del componente -->
+    <h1>HELLO WORLD</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  // Otras opciones y lógica del componente
+}
+</script>
+
+<style>
+/* Estilos del componente */
+</style>
